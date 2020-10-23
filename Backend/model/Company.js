81310ded4b8fc824340f144f17878f873f05cbb5
ApoorvMehrotra0706 <apoorv.mehrotra@sjsu.edu>
2020-10-22 23:40:33 -0700

    Adding required fields in company.js

--- Backend/model/Company.js
+++ Backend/model/Company.js
@@ -1,98 +1,104 @@
+const mongoose = require('mongoose');
 
-var mongoose = require('mongoose');
-
-var CompanySchema = new mongoose.Schema({
-    CompanyID: {type: String, required: true},
-    CompanyName: {type: String, required: true},
-    Website: {type: String, required: true},
-    Size: {type: Number, required: true},
-    ProfileImg: {type: String, required: true},
-    Type: {type: String, required: true},
-    Revenue: {type: Number, required: true},
-    Headquarter: {type: String, required: true},
-    Industry: {type: String, required: true},
-    Founded: {type: Date, required: true},
-    CompayDescription: {type: String, required: true},
-    CompanyMission: {type: String, required: true},
-    CEO: {type: String, required: true},
-    Viewcount: {type: Number, required: true},
-    SalaryReviewCount: {type: Number, required: true},
-    GeneralReviewCount: {type: Number, required: true},
-    InterviewReviewCount: {type: Number, required: true},
-    JobCount: {type: Number, required: true},
-    JobCount: {type: Number, required: true},
-    email: {type: String, required: true},
+const CompanySchema = new mongoose.Schema(
+  {
+    CompanyID: { type: String, required: true },
+    CompanyName: { type: String, required: true },
+    Website: { type: String },
+    Size: { type: Number },
+    ProfileImg: { type: String },
+    Type: { type: String },
+    Revenue: { type: Number },
+    Headquarter: { type: String },
+    Industry: { type: String },
+    Founded: { type: Date },
+    CompayDescription: { type: String },
+    CompanyMission: { type: String },
+    CEO: { type: String },
+    ViewCount: { type: Number },
+    SalaryReviewCount: { type: Number },
+    GeneralReviewCount: { type: Number },
+    InterviewReviewCount: { type: Number },
+    JobCount: { type: Number },
+    PhotoCount: { type: Number },
     FeaturedReview: [
-	    {
-	    	ReviewObject: Schema.Types.Mixed
-	    }
-    ].
-    SalaryReview : [
-    	{
-    		Status: {
-		        type: String,
-		        enum : [
-	        		'Not Approved',
-	        		'Approved', 
-	        		'Disapproved',
-		        ]
-		    },
-		    DatePosted: Date,
-	        BaseSalary: Number,
-	        Bonuses: Number,
-	        JobTitle: String,
-	        StreetAddress: Number,
-	        State: String,
-	        Country: String,
-	        Zip: String,
-    	}
-
+      {
+        Status: {
+          type: String,
+          enum: ['NotApproved', 'Approved', 'Disapproved'],
+        },
+        CompanyName: { type: String },
+        StudentID: { type: Number },
+        Pros: { type: String },
+        Cons: { type: String },
+        Descriptions: { type: String },
+        Rating: { type: Number },
+        EmployeeStatus: {
+          type: String,
+          enum: ['Current', 'Former'],
+        },
+        Helpful: { type: Number },
+        CEOApproval: { type: Boolean },
+        JobType: {
+          type: String,
+          enum: ['FullTime', 'PartTime', 'Contract', 'Intern', 'Freelance'],
+        },
+        Recommended: { type: Boolean },
+        JobTitle: { type: String },
+        Headline: { type: Boolean },
+        DatePosted: { type: Date },
+        Response: { type: String },
+        Favorite: { type: Boolean },
+      },
     ],
-    InterviewReview : [
-    	{
-    		Status: {
-		        type: String,
-		        enum : [
-	        		'Not Approved',
-	        		'Approved', 
-	        		'Disapproved',
-		        ]
-		    },
-		    Helpful: Number,
-	        DatePosted: Date,
-	        OverallExperience: {
-		        type: String,
-		        enum : [
-	        		'Positive',
-	        		'Negative', 
-	        		'Neutral',
-		        ]
-		    },
-	        JobTitle: String,
-	        Description: String,
-	        Difficulty: {
-		        type: String,
-		        enum : [
-	        		'Easy',
-	        		'Average', 
-	        		'Difficult',
-		        ]
-		    },
-		    OfferStatus: {
-		        type: String,
-		        enum : [
-	        		'Rejected',
-	        		'Accepted', 
-		        ]
-		    },
-		    InterviewQuestions: String,
-	        Answers: String,
-    	}
+    SalaryReview: [
+      {
+        Status: {
+          type: String,
+          enum: ['Not Approved', 'Approved', 'Disapproved'],
+        },
+        DatePosted: { type: Date },
+        BaseSalary: { type: Number },
+        Bonuses: { type: Number },
+        JobTitle: { type: String },
+        Years: { type: Number },
+        StreetAddress: { type: String },
+        State: { type: String },
+        Country: { type: String },
+        Zip: { type: Number, min: 10000, max: 99999 },
+      },
     ],
-    Photos: [
-    	{PhotoURL: String}
-    ]
-});
+    InterviewReview: [
+      {
+        Status: {
+          type: String,
+          enum: ['Not Approved', 'Approved', 'Disapproved'],
+        },
+        Helpful: { type: Number },
+        DatePosted: { type: Date },
+        OverallExperience: {
+          type: String,
+          enum: ['Positive', 'Negative', 'Neutral'],
+        },
+        JobTitle: { type: String },
+        Description: { type: String },
+        Difficulty: {
+          type: String,
+          enum: ['Easy', 'Average', 'Difficult'],
+        },
+        OfferStatus: {
+          type: String,
+          enum: ['Rejected', 'Accepted'],
+        },
+        InterviewQuestions: { type: String },
+        Answers: { type: String },
+      },
+    ],
+    Photos: [String],
+  },
+  {
+    versionKey: false,
+  }
+);
 
-
-module.exports = mongoose.model('Company', CompanySchema);
+module.exports = mongoose.model('company', CompanySchema);
