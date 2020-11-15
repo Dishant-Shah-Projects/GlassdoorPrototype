7f003868aa70cea2faf7b79d787ea2df0b051173
Chongye jWang <wangchongye@126.com>
2020-11-14 21:54:48 -0800

    Update Redis

--- Backend/redisClient.js
+++ Backend/redisClient.js
@@ -1,15 +1,13 @@
+/* eslint-disable */
 const redis = require("redis");
-const { redisPort , redisHost} = require("./redis-config");
-
+const { redisPort ,redisHost} = require("./redis-config");
 const redisClient = redis.createClient(redisPort);
-
-redisClient.on("connect", (err) =>  {
-    if(err){
-        console.log("Error while connecting to Redis server");
-    }
-    else {
-        console.log("Redis Server Connected");
-    }
+redisClient.on("connect", (err) => {
+  if (err) {
+    console.log("Error while connecting to Redis server");
+  } else {
+    console.log("Redis Server Connected");
+  }
 });
 
 module.exports = redisClient;
