748640ba4095fd0882e011891a0239252d8647ab
dshah9542 <dshah9542@gmail.com>
2020-11-16 15:41:58 -0800

    changes for kafka

--- Backend/redis-config.js
+++ Backend/redis-config.js
@@ -1,5 +1,7 @@
 module.exports = {
   // This is AWS Redis instance IP. Connect to this if you don't want to connect to local Redis
-  // redisHost: "123.123.123.123",
-  redisPort: 6379,
+  redisHost: 'redis-10162.c60.us-west-1-2.ec2.cloud.redislabs.com',
+  password: 'Dishant@1',
+  // redisPort: 6379,
+  redisPort: 10162,
 };
