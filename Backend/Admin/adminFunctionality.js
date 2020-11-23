89cc1a1204bd407b7c907e30d8bb6454ec361cc8
Chongye jWang <wangchongye@126.com>
2020-11-22 21:49:55 -0800

    Update Admin

---
+++ Backend/Admin/adminFunctionality.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-names */
+/* eslint-disable no-underscore-dangle */
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const url = require('url');
+
+const GeneralReview = require('../model/GeneralReview');
+const Company = require('../model/Company');
+
+// get reviews for the admin to approve
+const reviews = async (req, res) => {
+  try {
+    // eslint-disable-next-line object-curly-newline
+    const { PageNo } = url.parse(req.url, true).query;
+    const reviewsData = await GeneralReview.find({ Status: 'Not Approved' })
+      .limit(10)
+      .skip(PageNo * 10)
+      .exec();
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify(reviewsData));
+  } catch (error) {
+    res.writeHead(500, {
+      'Content-Type': 'application/json',
+    });
+    res.end('Network Error');
+  }
+};
+
+// Update review status
+const updateReviews = async (req, res) => {
+  try {
+    // eslint-disable-next-line object-curly-newline
+    const { CompanyID, ID, Status } = req.body;
+    const filter = { $and: [{ CompanyID }, { ID }] };
+    const update = { Status };
+    const result = await GeneralReview.findOneAndUpdate(filter, update);
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify(result));
+  } catch (error) {
+    res.writeHead(500, {
+      'Content-Type': 'application/json',
+    });
+    res.end('Network Error');
+  }
+};
+
+// get company list based on search
+const companyList = async (req, res) => {
+  try {
+    const { searchString, PageNo } = url.parse(req.url, true).query;
+    const results = await Company.find({ CompanyName: { $regex: `${searchString}`, $options: 'i' } })
+      .limit(10)
+      .skip(PageNo * 10)
+      .exec();
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify(results));
+  } catch (error) {
+    res.writeHead(500, {
+      'Content-Type': 'application/json',
+    });
+    res.end('Network Error');
+  }
+};
+
+module.exports = {
+  reviews,
+  updateReviews,
+  companyList,
+};
--- Backend/Routes/adminRoutes.js
+++ Backend/Routes/adminRoutes.js
@@ -1,5 +1,25 @@
 const express = require('express');
 
 const Router = express.Router();
+const {
+  reviews,
+  updateReviews,
+  companyList,
+} = require('../Admin/adminFunctionality');
+
+Router.get('/reviews', async (req, res) => {
+  const value = await reviews(req, res);
+  return value;
+});
+
+Router.post('/updateReviews', async (req, res) => {
+  const value = await updateReviews(req, res);
+  return value;
+});
+
+Router.get('/companyList', async (req, res) => {
+  const value = await companyList(req, res);
+  return value;
+});
 
 module.exports = Router;
