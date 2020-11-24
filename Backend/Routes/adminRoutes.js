3b2b0afc144622acc6f783c0e76d6bfb38832d53
Chongye jWang <wangchongye@126.com>
2020-11-23 16:22:45 -0800

    Update Passport

--- Backend/Routes/adminRoutes.js
+++ Backend/Routes/adminRoutes.js
@@ -1,23 +1,25 @@
 const express = require('express');
 
 const Router = express.Router();
+const { checkAuth } = require('../SharedFuntionalities/passport');
+
 const {
   reviews,
   updateReviews,
   companyList,
 } = require('../Admin/adminFunctionality');
 
-Router.get('/reviews', async (req, res) => {
+Router.get('/reviews', checkAuth, async (req, res) => {
   const value = await reviews(req, res);
   return value;
 });
 
-Router.post('/updateReviews', async (req, res) => {
+Router.post('/updateReviews', checkAuth, async (req, res) => {
   const value = await updateReviews(req, res);
   return value;
 });
 
-Router.get('/companyList', async (req, res) => {
+Router.get('/companyList', checkAuth, async (req, res) => {
   const value = await companyList(req, res);
   return value;
 });
