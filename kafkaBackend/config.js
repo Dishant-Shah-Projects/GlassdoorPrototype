f93806699a475197a4f0b1cc1b48c0b485bbddf7
dshah9542 <dshah9542@gmail.com>
2020-11-27 15:40:15 -0800

    Fixing Error

--- kafkaBackend/config.js
+++ kafkaBackend/config.js
@@ -4,6 +4,11 @@ const config = {
     'mongodb+srv://admin:ipPUEZQgUVOiIoz6@cluster0.dg0n5.mongodb.net/Glassdoor?retryWrites=true&w=majority',
   frontendURL: 'http://localhost:3000',
   kafkaport: 'localhost',
+  topic1: 'company2',
+  topic2: 'student2',
+  topic3: 'general2',
+  topic4: 'response_topic3',
+  topic5: 'admin2',
 };
 
 module.exports = config;
