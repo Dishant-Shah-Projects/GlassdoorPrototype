eb4509c44136e79fd7647806be8cc1f6c651c9f4
supriyasethi <60409351+supriyasethi@users.noreply.github.com>
2020-11-27 17:47:22 -0800

    company reviews

--- kafkaBackend/redisClient.js
+++ kafkaBackend/redisClient.js
@@ -1,20 +1,19 @@
-/* eslint-disable */
-const redis = require("redis");
-const { redisPort, redisHost } = require("./redis-config");
-//const redisClient = redis.createClient({ port: redisPort, host: redisHost });
-//redisClient.on('connect', (err) => {
+// const redis = require('redis');
+// const { redisPort, redisHost } = require('./redis-config');
+// const redisClient = redis.createClient({ port: redisPort, host: redisHost });
+// redisClient.on('connect', (err) => {
 //  if (err) {
 //    console.log('Error while connecting to Redis server');
 //  } else {
 //    console.log('Redis Server Connected');
 //  }
-//});
+// });
 
-const redisClient = require("redis-connection-pool")("myRedisPool", {
-  host: "54.193.50.216", // default
-  port: 6379, //default
-  max_clients: 200, // default
-  perform_checks: false, // checks for needed push/pop functionality
-});
-
-module.exports = redisClient;
+module.exports = {
+  // This is AWS Redis instance IP. Connect to this if you don't want to connect to local Redis	  // This is AWS Redis instance IP. Connect to this if you don't want to connect to local Redis
+  // redisHost: "123.123.123.123",
+  redisHost: 'redis-10162.c60.us-west-1-2.ec2.cloud.redislabs.com',
+  password: 'Dishant@1',
+  // redisPort: 6379,
+  redisPort: 10162,
+};
